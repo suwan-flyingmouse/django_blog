7ea8346ef73f524d9a3453e36d6cd4efecb241fe
GiDaeHyeon <spliter@naver.com>
2021-01-31 15:27:11 +0900

    [ADD] 대문페이지와 소개페이지 추가

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
+    path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
 ]
