45b2019b2b60f71f0ba654b6c80d3b694290e123
GiDaeHyeon <spliter@naver.com>
2021-01-31 14:07:27 +0900

    [ADD] Post모델 생성 후 시간 설정기능 추가

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,11 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # 장고의 모델을 만들면 자동으로 pk가 만들어진다.
+        # 이 값을 이용해 포스트의 제목과 번호를 문자열로 표현한다.
+        return f'{self.title}'
 # Create your models here.
