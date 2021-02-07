dff5e74da4b619807d40c306428f47ac00211591
GiDaeHyeon <spliter@naver.com>
2021-02-07 14:34:41 +0900

    [ADD] 포스트 상세 페이지 부트스트랩 적용

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,4 +120,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'),)
